90e3d3b932bb8aa40b07e495f22669a73dd36245
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-10-22 15:26:21 +0530

    splash icons and aap compiler issue fix

--- constants/index.ts
+++ constants/index.ts
@@ -1,9 +1,9 @@
 export const sliderImages = [
-  require("@/assets/images/slide1.png"),
-  require("@/assets/images/slide2.png"),
-  require("@/assets/images/slide3.png"),
-  require("@/assets/images/slide4.png"),
-  require("@/assets/images/slide5.png"),
+  require("@/assets/images/slide1.jpg"),
+  require("@/assets/images/slide2.jpg"),
+  require("@/assets/images/slide3.webp"),
+  require("@/assets/images/slide4.jpg"),
+  require("@/assets/images/slide5.jpg"),
 ];
     
 
